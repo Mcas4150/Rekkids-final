66f3d0fffdcd58cc96dc6ac964a3f2ca3b853c8a
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-10 15:57:31 -0400

    updated some views

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,19 +1,46 @@
 class RecordsController < ApplicationController
+  before_action :set_record, only: [:show, :edit, :update]
+
   def index
+    @records = Record.all
   end
 
   def show
   end
 
   def new
+    @record = Record.new
   end
 
   def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to record_path(@record)
+    else
+      render :new
+    end
+
   end
 
   def edit
   end
 
   def update
+    if @record.update(record_params)
+      redirect_to record_path(@record)
+    else
+      render :edit
+    end
   end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:name, :artist, :price, :user_id)
+  end
+
+  def set_record
+    @record = Record.find(params[:id])
+  end
+
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,6 @@
 class Record < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :artist, uniqueness: true, presence: true
+  validates :price, presence: true
 end
