0ddd6d173fa041d344d949c353465823eb188fc0
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-10 14:32:20 -0400

    added controllers

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+  def new
+  end
+
+  def crete
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+
+  get 'orders/crete'
+
+  get 'orders/index'
+
+  get 'orders/show'
+
+  get 'orders/delete'
+
+  get 'records/index'
+
+  get 'records/show'
+
+  get 'records/new'
+
+  get 'records/create'
+
+  get 'records/edit'
+
+  get 'records/update'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
