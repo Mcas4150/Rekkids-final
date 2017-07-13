1f4c85bd65370e20cb164a9ccab1ae50e3f71207
takahiro yokoyama <ytakahiro72@gmail.com>
2017-07-13 13:27:39 -0400

    added location data to each record

--- app/models/record.rb
+++ app/models/record.rb
@@ -7,4 +7,7 @@ class Record < ApplicationRecord
   has_attachment :photo
   validates :photo, presence: true
   validates :description, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
