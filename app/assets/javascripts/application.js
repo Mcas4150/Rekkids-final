decd54d2c79bd701bdfbe71791e72e51b2a7d0a4
takahiro yokoyama <ytakahiro72@gmail.com>
2017-07-13 12:05:18 -0400

    I added map function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require_tree .
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require underscore
+//= require gmaps/google
+//= require_tree .
 
 $(document).ready(function() {
   $('.attachinary-input').attachinary();
 });
+
