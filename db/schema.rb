87f053d439343319a31f9609dd5131a28fb3146e
Scott Matheson <scott.louis.math@gmail.com>
2017-07-14 13:37:47 -0400

    updated date picker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713200133) do
+ActiveRecord::Schema.define(version: 20170714155500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20170713200133) do
     t.integer  "record_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "from"
+    t.date     "to"
     t.index ["record_id"], name: "index_orders_on_record_id", using: :btree
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
