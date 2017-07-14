eb7a6f3a502e85a05a562ff678bc647f56c7e827
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-14 15:50:33 -0400

    tweek

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook]
 
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
   private
 
