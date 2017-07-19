75a224ee2a64f329a3b997b101699d3a6031d796
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-19 17:53:40 -0400

    discogs

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,47 @@
 class RecordsController < ApplicationController
   before_action :set_record, only: [:show, :edit, :update]
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_filter do
+    @discogs = Discogs::Wrapper.new("REKKIDS", session[:access_token])
+  end
+
+
+  def authenticate
+    app_key = ENV["FBKQYVnAGfObiPrBFrhe"]
+    app_secret = ENV["OdekVDfPyPPgarzrpUpDqXCFjShBWjdk"]
+  @discogs     = Discogs::Wrapper.new("REKKIDS")
+  request_data = @discogs.get_request_token(app_key, app_secret, "http://127.0.0.1:3000/callback")
+
+  session[:request_token] = request_data[:request_token]
+
+  redirect_to request_data[:authorize_url]
+end
+
+# And an action that Discogs will redirect back to.
+def callback
+  @discogs      = Discogs::Wrapper.new("REKKIDS")
+  request_token = session[:request_token]
+  verifier      = params[:oauth_verifier]
+  access_token  = @discogs.authenticate(request_token, verifier)
+
+  session[:request_token] = nil
+  session[:access_token]  = access_token
+
+  @discogs.access_token = access_token
+
+  # You can now perform authenticated requests.
+end
+
+
+
+
+
+
+
+
+
+
+
 
   def index
     @records = Record.all
