7173eb7a82a48be65e6c70a70c5b5689b21243c2
Michael Cassidy <mikecassidy0000@gmail.com>
2017-07-27 21:35:41 -0400

    lets pray

--- db/migrate/20170710181916_create_records.rb
+++ db/migrate/20170710181916_create_records.rb
@@ -3,7 +3,6 @@ class CreateRecords < ActiveRecord::Migration[5.0]
     create_table :records do |t|
       t.string :name
       t.integer :price
-      t.primary_key :user_id
       t.references :user, foreign_key: true
 
       t.timestamps
